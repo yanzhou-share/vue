85c0a0cdedbdb92fab897e7bfe247be1e7147b17
单衍州 <share_yzh@163.com>
2022-03-31 09:39:40 +0800

    vue 滚动条

---
+++ 返回记住滚动条位置.vue
@@ -0,0 +1,24 @@
+<template>
+    <keep-alive>
+        <router-view></router-view>
+    </keep-alive>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            scrollTop: 0
+        }
+    },
+    beforeRouteLeave(){
+        //注意滚动条的对象是谁？有可能是body 也有可能是你自定义产生的滚动条
+        let app = document.getElementById('app')
+        this.scrollTop = app.scrollTop || 0
+        next()
+    },
+    activated() {
+        document.getElementById('app').scrollTop = this.scrollTop
+    }
+}
+</script>
